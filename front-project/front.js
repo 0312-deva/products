d812d661abfb7d51c472d006c92a80f733ee5db0
unknown <devadharshini3122002@gmail.com>
2024-03-29 15:13:19 +0530

    rename

--- front-project/front.js
+++ front-project/front.js
@@ -3,7 +3,7 @@ containerDiv.classList.add('containerStyle')
 
 
 window.addEventListener('load',()=> 
-fetch(`https://products-2.onrender.com`)
+fetch(`http://localhost:3000/products`)
 .then(res =>res.json())
 .then(products=> generateUI(products)))
 
